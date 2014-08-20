b166eb632e5736ef1d42580138cefb55558661f7
friedpine <friedpine@gmail.com>
2014-08-20 15:53:06 +0800

    Update module12_short.py

--- module12_short.py
+++ module12_short.py
@@ -32,6 +32,20 @@ def adaptor_pair2_pos(seq):
   return pos
 
 def seq_overlaption(seq1,seq2,qua1,qua2):
+  print "NICE!"
+
+def pairs_merge_by_adaptor(seq1,seq2,qua1,qua2):
+  seq1,qua1 = remove_low_quantity(seq1,qua1)
+  seq2,qua2 = remove_low_quantity(seq2,qua2)
+  adp_pos_1 = adaptor_pair1_pos(seq1)
+  adp_pos_2 = adaptor_pair2_pos(seq2)
+  adp_pos = max(adp_pos_1+adp_pos_2)
+  if adp_pos > 0:
+    return 1,seq1[0:adp_pos],qua1[0:adp_pos]
+  else:
+    return 0,"NA","NA"
+
+def pair_merge_by_overlap(seq1,seq2,qua1,qua2):
   seq1,qua1 = remove_low_quantity(seq1,qua1)
   seq2,qua2 = remove_low_quantity(seq2,qua2)  
   seq2 = reverse_complementary(seq2)
@@ -49,23 +63,7 @@ def seq_overlaption(seq1,seq2,qua1,qua2):
     return 1,seq1+seq2[len(seq1)-pos:len(seq2)],qua1+qua2[len(seq1)-pos:len(seq2)]
   else:
     return 0,seq1,seq2
-
-seq_overlaption(seq1,seq2,qua1,qua2)    
-
-def pairs_merge_by_adaptor(seq1,seq2,qua1,qua2):
-  seq1,qua1 = remove_low_quantity(seq1,qua1)
-  seq2,qua2 = remove_low_quantity(seq2,qua2)
-  adp_pos_1 = adaptor_pair1_pos(seq1)
-  adp_pos_2 = adaptor_pair2_pos(seq2)
-  adp_pos = max(adp_pos_1+adp_pos_2)
-  if adp_pos > 0:
-    return 1,seq1[0:adp_pos],qua1[0:adp_pos]
-  else:
-    return 0,"NA","NA"
-
-def pair_merge_by_overlap(seq1,seq2,qua1,qua2):
   
-
 seq1 = "CACACCGGTCGCAGCTGCTCCGGCACCGGTACTGCCGGCGCGCCGGCAAACGTCTTAGATCGGAAGAGCACACGTCTGAACTCCAGTCACGTGAAAATCTC"
 qua1 = "@@CFFFFFCCDDHGGHIIBHBGGIEHIGGIHHIIIIIHFCCBBBBBBCCCB@AAABACCCCB7?BB<AC>ACC>??9@CCCCCCCCC>CCBAAB0>:4:>C"
 seq2 = "AAGACGTTTGCCGGCGCGCCGGCAGTACCGGTGCCGGAGCAGCTGCGACCGGTGTGAGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGG"
@@ -74,9 +72,8 @@ qua2 = "@@@?DDADFHFFF@@@DAG@DHIID@A>EEEDBACBBBB;9C?C@>9@<<BB5<6?3@CCC@B2?B@BCB@?
 seq1 = "CAAATCATATTATAGGCATGTGTGTGCAGTTGGTGGTGTATTAAATTTACTCACTATGTGGGCTGCAAACTTAGTAGGATTTGCTGTTGGGTTGGATGGTA"
 seq2 = "AAAATCAAACTTATTTTATACTGACCATCTGACGTTCCAAAAATATTACTTAATAATGATTTCATACCATCCAACCCAACAGCAAATCCTACTAAGTTTGC"
 
-seq_overlaption(seq1,seq2,qua1,qua2)
-
-pairs_merge_by_adaptor(seq1,seq2,qua1,qua2)
+#pair_merge_by_overlap(seq1,seq2,qua1,qua2)
+#pairs_merge_by_adaptor(seq1,seq2,qua1,qua2)
   
 
 
