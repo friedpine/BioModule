f48126e4d460e3df6cc21b0ff172c8de209a5318
friedpine <friedpine@gmail.com>
2014-08-22 12:23:54 +0800

    Update module13_CNV.py

--- module13_CNV.py
+++ module13_CNV.py
@@ -1,6 +1,7 @@
 import pysam
 import os, sys, re
 import MySQLdb as mb
+import infra05_MySQLDB as in05
 
 conn=mb.connect(host="localhost",user="root",passwd="123456",db='lihang')
 cursor = conn.cursor()
@@ -27,3 +28,32 @@ def GET_BINNED_COUNTS_ALONG_GENOME(cursor,conn,bamfile,binsize,chrom_size,tablen
   conn.commit()
   
       
+def GET_BINNED_COUNTS_ALONG_GENOME_Multiple(cursor,conn,samples,bamfile,binsize,chrom_size,tablename):
+  in05.CREATE_TABLE(cursor,tablename,['chr','bin_id']+colnames,['varchar(20)','int']+['int']*len(samples))
+  cursor.execute("select chr,`length` from "+chrom_size)
+  results = cursor.fetchall()
+  
+  samfiles = []
+  for sample in samples:
+    bamname = d00.get_sample_file(cursor,sample,bamfile)
+    samfiles.append(pysam.Samfile(bamname, "rb"))
+  
+  counts = []
+  for chr in results:
+    for pos in range(1,int(chr[1]),binsize):
+      bin_info = [chr[0],int(pos/binsize)]
+      for samfile in samfiles:
+        bin_info.append(samfile.count(chr[0],pos,min(pos+binsize,int(chr[1]))))
+      counts.append(counts)
+  return counts[1]
+    
+      
+      
+      
+      counts.append(bin_info)
+  try:
+    cursor.execute(sql_table)
+  except:
+    print "EXISTS"
+  cursor.executemany("insert ignore into "+tablename+" values(%s,%s,%s) """,counts)
+  conn.commit()
