f565e2c984d7f866c26a8d4815857735d6622ccd
friedpine <friedpine@gmail.com>
2014-08-24 23:13:58 +0800

    Update module11_VCF.py

--- module11_VCF.py
+++ module11_VCF.py
@@ -124,7 +124,7 @@ def read_VCF_file_single(cursor,conn,DB_NAME,tablename,samples,type):
 def Group_Samples_VCF_from_Bam_rmd(cursor,conn,samples,group_name,file_type,SNV_type,SNV_process_type,outdir,cmd_file):
 	m00.FILES_GROUPER(cursor,conn,samples,group_name,file_type,' ')
 	w1 = m00.COMMAND_generator(cursor,conn,[group_name],"samtools mpileup -Duf /data/Analysis/fanxiaoying/database/hg19/00.genome/genome.fa #0 | /data/Analysis/fanxiaoying/software/samtools-0.1.19/bcftools/bcftools view -bvcg -> #1",[file_type],outdir,'.bcf',SNV_type)
-	w2 = m00.COMMAND_generator(cursor,conn,[group_name],"/data/Analysis/fanxiaoying/software/samtools-0.1.19/bcftools/bcftools view #0 | perl /data/Analysis/fanxiaoying/project/project01_polyA-RNAseq/modules/scripts/vcfutils.pl varFilter -d 5 >#1",['SNV'],outdir,'.vcf',SNV_process_type)
+	w2 = m00.COMMAND_generator(cursor,conn,[group_name],"/data/Analysis/fanxiaoying/software/samtools-0.1.19/bcftools/bcftools view #0 | perl /data/Analysis/fanxiaoying/project/project01_polyA-RNAseq/modules/scripts/vcfutils.pl varFilter -d 5 >#1",SNV_type,outdir,'.vcf',SNV_process_type)
 	m00.w(w1+w2,cmd_file)
 	
 def Single_Sample_SNV_FILE(cursor,conn,sample,fq_type,bam_type,bam_type_rmd,SNV_type,SNV_process_type,outdir,cmd_file):
