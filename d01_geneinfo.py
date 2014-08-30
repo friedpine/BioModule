9fbdaced5e18107cb3a6806ab3484377955d7fa7
friedpine <friedpine@gmail.com>
2014-08-30 20:47:59 +0800

    mm10_ENSG_3UTR

--- d01_geneinfo.py
+++ d01_geneinfo.py
@@ -2,5 +2,5 @@ import MySQLdb as mb
 import re,os
 
 def mm10_ENSG_3UTR(cursor,conn,genename,flank_len):
-	cursor.execute("select * from mm10_ensembl.UTR3 where genename = %s",([genename]))
-	return cursor.fetchall()[0][0]
+	cursor.execute("select chr,strand,left_pos,right_pos from mm10_ensembl.UTR3 where genename = %s",([genename]))
+	return cursor.fetchall()[0]
