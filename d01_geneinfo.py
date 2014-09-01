6cafdcad2d4d8e05ae09fac3d7595d62ad05a4ee
friedpine <friedpine@gmail.com>
2014-09-01 11:32:15 +0800

    mm10_refGene_3UTR:Flank and returns a dict!

--- d01_geneinfo.py
+++ d01_geneinfo.py
@@ -3,5 +3,18 @@ import re,os
 
 def mm10_refGene_3UTR(cursor,conn,genename,flank_len):
 	cursor.execute("select transc,chr,strand,`left`,`right` from mm10_refseq.utr3 where gene = %s",([genename]))
-	return cursor.fetchall()[0]
-		
+	outs = {}
+	for x cursor.fetchall():
+		pos = str(x[3])+'_'+str(x[4])
+		if pos in outs:
+			continue
+		else:
+			if x[2] == "+":
+				range_flank = [x[3],x[4]+flank_len]
+			if x[2] == "-":
+				range_flank = [x[3]-flank_len,x[4]]
+			outs[pos] = {'chr':x[1],'range':[x[3],x[4]],'range_flank':range_flank,'strand':x[2],'transc':x[0]}
+	return outs	
+				
+def PLOT_APA_Predicted():
+	print "HEHE"
