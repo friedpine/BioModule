e015028be37d537b2e665ba00a0192d63afc5bf1
friedpine <friedpine@gmail.com>
2014-09-18 15:07:14 +0800

    m14

--- module14_APA.py
+++ module14_APA.py
@@ -224,14 +224,19 @@ def Downhills_Intermediate(cursor,conn,samples,bam_handles,genename,flanksize,mi
 #OUTPUTS:A copyed table of the input table but with the detailed 
 def Relative_APA_Site_Usage(cursor,conn,samples,bamfiles,sourcetable,outtable,width,chr_phase):
 	try:
-		cursor.execute("create table "+outtable+" as select * from "+sourcetable+" limit 10")
+		cursor.execute("create table "+outtable+" as select * from "+sourcetable)
 	except:
 		print "EXISTS"	
-	cursor.execute("select * from "+sourcetable+" limit 10")
+	cursor.execute("select * from "+sourcetable)
 	sites = cursor.fetchall()
+	ids = []
+	samples = [x.replace('-','_') for x in samples]
+	print samples
 	colnames = ['near_'+x for x in samples]+['dist_'+x for x in samples]
 	coldata = [[] for x in colnames]
 	for site in sites:
+		ids.append(site[0])
+		site = site[1:]
 		if site[2] == '+':
 			position_near = [site[1][chr_phase:],site[3]-width,site[3]]
 			position_dist = [site[1][chr_phase:],site[4]-width,site[4]]
@@ -243,7 +248,9 @@ def Relative_APA_Site_Usage(cursor,conn,samples,bamfiles,sourcetable,outtable,wi
 		for index,data in enumerate(m02.Depth_Range_Mean(samples,bamfiles,position_dist)):
 			coldata[index+len(samples)].append(data)
 	print colnames,coldata
-	d02.append_colume_info_to_tables(cursor,conn,outtable,colname,colinfo,ids,seqs)
+	for x,colname in enumerate(colnames):
+		print outtable,colname,'float',ids,coldata[x]
+		d02.append_colume_info_to_tables(cursor,conn,outtable,colname,'float',ids,coldata[x])
 	
 
 
